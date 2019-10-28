c786470c7f0fbaa4f87e5abfc6a55d9ae59f955e
DJHunn39 <dannyjohnhunn@yahoo.co.uk>
2019-10-28 23:38:13 +0000

    Clean out master

--- index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import DJHunnHomepage from './src/client/DJHunnHomepage';
-
-const container = document.querySelector('#djhunn39-homepage');
-
-ReactDOM.render(<DJHunnHomepage />, container);
--- src/client/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Home = () => {
-  return(
-    <p>I'm gonna put something here, I promise.</p>
-  )
-}
-
-export default Home;
